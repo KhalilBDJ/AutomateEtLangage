226a58c586198198b4826cb37c9d53a164ce18c5
frderoubaix <francois.deroubaix@outlook.fr>
2022-05-10 15:53:32 +0200

    add gitignore + add first step of automaton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import parser.JsonAutomaton;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        JsonAutomaton jsonAutomaton = new JsonAutomaton();
+        jsonAutomaton.decodeJson();
+    }
 }
--- src/main/java/parser/JsonAutomaton.java
+++ src/main/java/parser/JsonAutomaton.java
@@ -1,4 +1,28 @@
 package parser;
 
-public class JsonAutomaton {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+public class JsonAutomaton extends Automaton {
+    private final InputStream file;
+
+    public JsonAutomaton(){
+        file = Objects.requireNonNull(getClass().getResourceAsStream("/bus.json"));
+    }
+
+    public void decodeJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode bus = mapper.readTree(file);
+
+        System.out.println(line(bus));
+
+    }
+
+    public String line(JsonNode jsonNode){
+        return jsonNode.get("ligne").asText();
+    }
 }
