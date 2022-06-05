de2696788901a6ec482ce2d538bc4b55b5346cc3
Khabizard <khalilobedjaoui@gmail.com>
2022-06-05 22:19:23 +0200

    Update for PC Change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,20 @@
 import org.xml.sax.SAXException;
 import parser.JsonAutomaton;
 import parser.XMLAutomaton;
+import parser.TextAutomaton;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
-        JsonAutomaton jsonAutomaton = new JsonAutomaton();
-        jsonAutomaton.decodeJson();
+        //JsonAutomaton jsonAutomaton = new JsonAutomaton();
+        //jsonAutomaton.decodeJson();
 
-        XMLAutomaton xmlAutomaton = new XMLAutomaton();
-        xmlAutomaton.defineLines();
+        //XMLAutomaton xmlAutomaton = new XMLAutomaton();
+        //xmlAutomaton.defineLines();
+
+        TextAutomaton textAutomaton = new TextAutomaton();
+        textAutomaton.GetLiaisons();
     }
 }
