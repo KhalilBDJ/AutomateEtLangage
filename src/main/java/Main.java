57d1162b60889b0f83916ae7e85f7c0e15622571
Khabizard <khalilobedjaoui@gmail.com>
2022-06-17 12:44:02 +0200

    Update for PC Change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ import parser.TextAutomaton;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
@@ -24,6 +26,13 @@ public class Main {
         //textAutomaton.createRoute();
 
         TextAutomatonBis automatonBis = new TextAutomatonBis();
-        automatonBis.setStation();
+        automatonBis.setRules();
+
+        /*String test = "% départ de Gare toutes les 10 minutes de 07:00 à 09:00 et de 16:30à 18:00";
+        String test2 = test.replaceAll("[A-Za-zùàé%]", "").replaceAll(":", "");
+        System.out.println(test2);
+        List<String> list= new ArrayList<>(List.of(test2.split("\\s+")));
+        list.remove(0);
+        System.out.println(list);*/
     }
 }
--- src/main/java/parser/TextAutomatonBis.java
+++ src/main/java/parser/TextAutomatonBis.java
@@ -1,9 +1,12 @@
 package parser;
 
+import Metro.Metro;
 import model.*;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,6 +16,9 @@ public class TextAutomatonBis{
 
     protected File file;
     protected Line line;
+
+    public Metro.TempsArret arret;
+    public List<String> tempsDepart = new ArrayList<>();
     public TextAutomatonBis(){
        file = new File("./src/main/resources/metro.txt");
        line = new Line(Transport.METRO);
@@ -43,6 +49,57 @@ public class TextAutomatonBis{
         }
         System.out.println(stations);
         return stations;
+    }
+
+    public List<List<String>> setRules() throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        List<List<String>> rules = new ArrayList<>();
+        while (scanner.hasNextLine()){
+
+            String strline = scanner.nextLine();
+            if (Pattern.matches("% arrêt de .*",strline)){
+                arret = new Metro.TempsArret(Integer.parseInt(strline.replaceAll("[A-Za-zâê% ]", "").replaceAll(":", "")));
+
+            }
+            if (Pattern.matches("% départ de .*", strline)){
+                int i = 0;
+                List<String> rule = new ArrayList<>(List.of(strline.replaceAll("[A-Za-zùàé%]", "").replaceAll(":", "").split("\\s+")));
+                rule.remove(0);
+                for (String simple : rule) {
+                    if (simple.length()<=2){
+                        tempsDepart.add(simple);
+                        break;
+                    }
+                    i++;
+                }
+                if (i<= rule.size()-1){
+                    rule.remove(i);
+                }
+                rules.add(rule);
+            }
+            if (Pattern.matches("% dernier départ .*",strline)){
+                int i =0;
+                List<String> rule = new ArrayList<>(List.of(strline.replaceAll("[A-Za-zùàé%]", "").replaceAll(":", "").split("\\s+")));
+                rule.remove(0);
+                for (String simple : rule) {
+                    if (simple.length()<=2){
+                        tempsDepart.add(simple);
+                        break;
+                    }
+                    i++;
+                }
+                if (i<= rule.size()-1){
+                    rule.remove(i);
+                }
+
+                rules.add(rule);
+            }
+
+        }
+        System.out.println(rules);
+        System.out.println(arret.getA());
+        System.out.println(tempsDepart);
+        return rules;
     }
 
 
