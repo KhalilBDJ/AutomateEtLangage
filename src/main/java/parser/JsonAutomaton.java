b9c96709d6810540a1d15e008b0b1177b42b3a31
frderoubaix <francois.deroubaix@outlook.fr>
2022-06-27 19:39:16 +0200

    refacto all Automaton based on abstract class

--- src/main/java/parser/JsonAutomaton.java
+++ src/main/java/parser/JsonAutomaton.java
@@ -1,8 +1,8 @@
 package parser;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import exception.BadValueException;
 import model.*;
 
 import java.io.IOException;
@@ -10,60 +10,68 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class JsonAutomaton extends Automaton<JsonNode> {
-    public JsonAutomaton(String filename){
+public class JsonAutomaton extends Automaton {
+
+    Network network;
+    private JsonNode bus;
+    Line line;
+
+    public JsonAutomaton(Network network, String filename) throws IOException {
         file = Objects.requireNonNull(getClass().getResourceAsStream(filename));
-        line = new Line(Transport.CITY_BUS);
-    }
-
-    public JsonAutomaton(){
-        file = Objects.requireNonNull(getClass().getResourceAsStream("/bus.json"));
-        line = new Line(Transport.CITY_BUS);
-    }
-
-
-    //TODO: rework plus besoin de passer un JsonNode dans les méthodes !!
-    public void decodeJson() throws IOException {
-        //TODO : mettre ça en attribut
+        line = new Line(Transport.BUS);
+        this.network = network;
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode bus;
-        try{
-             bus = mapper.readTree(file);
-        }catch (JsonParseException e){
+        try {
+            bus = mapper.readTree(file);
+        } catch (IOException e) {
             System.err.println("Une erreur a été détecté lors de la lecture du fichier json : ");
             throw e;
         }
-
-        line.setName(getLineName(bus));
-        line.setStations(getLineStations(bus));
-        line.setDirections(getDirections(bus));
+        line.setName(getLineName());
     }
 
-    public String getLineName(JsonNode jsonNode){
-        return jsonNode.get("ligne").asText();
+    public JsonAutomaton(Network network) throws IOException {
+        this(network, "/bus.json");
     }
 
-    public List<Station> getLineStations(JsonNode jsonNode){
+    @Override
+    public void addNetworkGlobalStations() {
+        for (JsonNode horaire : bus.get("horaires")) {
+            for (JsonNode station : horaire.get("stations")) {
+                Station startStation = network.getStationWithName(station.get("station").asText());
+                if (startStation == null) {
+                    network.addStation(new Station(station.get("station").asText(), false));
+                }
+            }
+        }
+    }
+
+    public String getLineName() {
+        return bus.get("ligne").asText();
+    }
+
+    public List<Station> getStationsFromLine() {
         List<Station> stations = new ArrayList<>();
-        JsonNode jsonStations = jsonNode.get("horaires").get(0).get("stations");
+        JsonNode jsonStations = bus.get("horaires").get(0).get("stations");
         int stationIndex = 0;
-        for (JsonNode station : jsonStations){
-            boolean isTerminus = stationIndex == 0 || stationIndex == jsonStations.size()-1;
+        for (JsonNode station : jsonStations) {
+            boolean isTerminus = stationIndex == 0 || stationIndex == jsonStations.size() - 1;
             stations.add(new Station(station.get("station").asText(), isTerminus));
             stationIndex++;
         }
         return stations;
     }
 
-    public List<Direction> getDirections(JsonNode jsonNode){
+    public void defineLines() throws BadValueException {
         List<Direction> directions = new ArrayList<>();
-        JsonNode jsonDirections = jsonNode.get("horaires");
-        for (JsonNode direction : jsonDirections){
+        JsonNode jsonDirections = bus.get("horaires");
+        line.setStations(getStationsFromLine());
+        for (JsonNode direction : jsonDirections) {
             Station terminusStation = line.getStationWithName(direction.get("direction").asText());
             JsonNode passages = direction.get("passages");
             JsonNode jsonStations = direction.get("stations");
             int stationIndex = 0;
-            for (JsonNode station : jsonStations){
+            for (JsonNode station : jsonStations) {
                 Station currentStation = line.getStationWithName(station.get("station").asText());
 
                 Direction dir = new Direction();
@@ -81,15 +89,15 @@ public class JsonAutomaton extends Automaton<JsonNode> {
                 stationIndex++;
             }
         }
-        System.out.println(directions);
-        return directions;
+        line.setDirections(directions);
     }
 
-    public List<Route> createRoute(){
+    public List<Route> createRoute() throws BadValueException {
+        defineLines();
         List<Route> routes = new ArrayList<>();
-        for (Direction direction : line.getDirections()){
+        for (Direction direction : line.getDirections()) {
             long routeDuration = line.durationBetweenNextStation(direction);
-            if (routeDuration != 0){
+            if (routeDuration != 0) {
                 Route route = new Route(routeDuration, direction, Transport.BUS);
                 routes.add(route);
             }
@@ -97,7 +105,7 @@ public class JsonAutomaton extends Automaton<JsonNode> {
         return routes;
     }
 
-    public void addToNetwork(Network network){
+    public void addToNetwork(Network network) throws BadValueException {
         network.addRoutes(createRoute());
         network.addLine(line);
     }
